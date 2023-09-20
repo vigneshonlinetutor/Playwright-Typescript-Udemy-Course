e0ad38ea35fc236475dd0b3bff4dec2c0c7bcea4
Vignesh Srinivasa Raghavan <66298813+vigneshonlinetutor@users.noreply.github.com>
2023-09-20 12:17:00 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,6 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    headless:false,
     trace: 'on-first-retry',
   },
 

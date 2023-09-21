0967044896ba62a2ef5716246602ab2fe90a1d86
Vignesh Srinivasa Raghavan <66298813+vigneshonlinetutor@users.noreply.github.com>
2023-09-21 22:28:14 +0200

    Added Alert test

---
+++ tests/alertHandling.spec.ts
@@ -0,0 +1,66 @@
+import { test, expect } from '@playwright/test'
+
+test('Simple Alert Handling', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+
+    page.on("dialog",async(alert)=>{
+        const alertMessage = alert.message();
+        expect(alertMessage).toEqual('I am a JS Alert Vignesh');
+        await alert.accept();
+        await expect(page.locator('#result')).toHaveText('You successfully clicked an alert');
+    })
+    await page.locator('button[onclick="jsAlert()"]').click();
+    await page.close();
+});
+
+test('Confirm Alert - Ok Button', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+
+    page.on("dialog",async(alert)=>{
+        const alertMessage = alert.message();
+        expect(alertMessage).toEqual('I am a JS Confirm');
+        await alert.accept();
+        await expect(page.locator('#result')).toHaveText('You clicked: Ok');
+    })
+    await page.locator('button[onclick="jsConfirm()"]').click();
+    await page.close();
+});
+
+test('Confirm Alert - Cancel Button', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+
+    page.on("dialog",async(alert)=>{
+        const alertMessage = alert.message();
+        expect(alertMessage).toEqual('I am a JS Confirm');
+        await alert.dismiss();
+        await expect(page.locator('#result')).toHaveText('You clicked: Cancel');
+    })
+    await page.locator('button[onclick="jsConfirm()"]').click();
+    await page.close();
+});
+
+test('Prompt Alert - Ok Button', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+
+    page.on("dialog",async(alert)=>{
+        const alertMessage = alert.message();
+        expect(alertMessage).toEqual('I am a JS prompt');
+        await alert.accept("Playwright");
+        await expect(page.locator('#result')).toHaveText('You entered: Playwright');
+    })
+    await page.locator('button[onclick="jsPrompt()"]').click();
+    await page.close();
+});
+
+test('Prompt Alert - Cancel Button', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+
+    page.on("dialog",async(alert)=>{
+        const alertMessage = alert.message();
+        expect(alertMessage).toEqual('I am a JS prompt');
+        await alert.dismiss();
+        await expect(page.locator('#result')).toHaveText('You entered: null');
+    })
+    await page.locator('button[onclick="jsPrompt()"]').click();
+    await page.close();
+});

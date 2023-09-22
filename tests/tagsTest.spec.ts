71321874703283c883a2ee87af649521b39e517a
Vignesh Srinivasa Raghavan <66298813+vigneshonlinetutor@users.noreply.github.com>
2023-09-22 19:55:49 +0200

    Added Tags Test

---
+++ tests/tagsTest.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('Login Page Test @smoke',async () => {
+  console.log("This is a Login Page test")
+});
+
+test('Home Page Test @sanity',async () => {
+  console.log("This is a Home Page test")
+});
+
+test('Payment Page Test @regression',async () => {
+  console.log("This is a Payment Page test")
+});
+
+test('Checkout Page Test @regression',async () => {
+  console.log("This is a Checkout Page test")
+});
